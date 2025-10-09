978f90adfa169d17b81f2c345071107354bf65a7
adan0126 <mgranadosl2301@alumno.ipn.mx>
2025-10-08 18:55:51 -0600

    Más problemas :'c (el profesor va a resolver el problema #2)

---
+++ 06Algoritmia/JS/algoritmia.js
@@ -0,0 +1,11 @@
+function resolverProblema1() {
+    // Problema 1: Invertir el orden de una serie de palabras
+}
+
+function resolverProblema2() {
+    // Problema 2: Producto esclar de dos vectores
+}
+
+function resolverProblema3() {
+    // Problema 3: Saber el número de caracteres unicos de una serie de palabras
+}
