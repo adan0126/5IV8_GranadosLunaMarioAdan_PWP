cf2063c46e2db01f300ceb97b38140c78147c8c5
adan0126 <mgranadosl2301@alumno.ipn.mx>
2025-10-11 14:45:53 -0600

    Se termino el js para el ejercicio2, se hicieron pruebas y parece que funcionan

--- 05Ejercicios/JS/funciones2.js
+++ 05Ejercicios/JS/funciones2.js
@@ -5,3 +5,28 @@ function validarSalario(e) { // e = evento
     var prueba = String.fromCharCode(teclado);
     return patron.test(prueba);
 }
+
+function validarVentas(e) { // e = evento
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9]/;
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+function calcular() {
+    var salario = document.getElementById("salario").value;
+    var parseoSalario = parseFloat(salario);
+    var ventas = document.getElementById("ventas").value;
+    var parseoVentas = parseInt(ventas);
+    var comision = parseoVentas * 0.1;
+    var total = parseoSalario + comision;
+    alert("El total de su salario es: " + total.toFixed(2) + " pesos");
+    document.getElementById("total").value = total.toFixed(2) + " pesos";
+}
+
+function borrar() {
+    document.getElementById("salario").value = "";
+    document.getElementById("ventas").value = "";
+    document.getElementById("total").value = "";
+}
