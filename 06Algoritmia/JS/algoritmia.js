5bdde2af7aac9a1841dc3a8ef56d7773ace064b6
adan0126 <mgranadosl2301@alumno.ipn.mx>
2025-10-12 15:41:35 -0600

    Despues de tantas pruebas y tantos intentos alfin se termino el js para 06algoritmia

--- 06Algoritmia/JS/algoritmia.js
+++ 06Algoritmia/JS/algoritmia.js
@@ -1,11 +1,40 @@
+function evento1(e){
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[A-Za-z ]/;
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
 function resolverProblema1() {
     // Problema 1: Invertir el orden de una serie de palabras
+    const input = document.getElementById('p1-input').value;
+    const palabras = input.split(' ');
+    const palabrasInvertidas = palabras.reverse();
+    const resultado = palabrasInvertidas.join(' ');
+    document.getElementById('p1-output').innerText = resultado;
 }
 
 function resolverProblema2() {
     // Problema 2: Producto esclar de dos vectores
 }
 
+function evento3(e){
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[A-Z\,]/;
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
 function resolverProblema3() {
     // Problema 3: Saber el número de caracteres unicos de una serie de palabras
+    const input = document.getElementById('p3-input').value;
+    const palabras = input.split(',');
+    let resultado = '';
+    palabras.forEach(palabra => {
+        const caracteresUnicos = new Set(palabra).size;
+        resultado += `La palabra "${palabra}" tiene ${caracteresUnicos} caracteres únicos.\n`;
+    });
+    document.getElementById('p3-output').innerText = resultado;
 }
