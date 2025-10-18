667bcd668a9407584fd4b49d07b9f48fb477ee3b
adan0126 <mgranadosl2301@alumno.ipn.mx>
2025-10-18 16:29:37 -0600

    Se agregaron la practica 8 y 7 al index principal, además se termino el js validado y funcional para la practica 7

--- 07JSintermedio/funcionesintermedio.js
+++ 07JSintermedio/funcionesintermedio.js
@@ -26,7 +26,7 @@ console.log(suma(2,3)); // 5
 const suma = (n1, n2) => n1 + n2;
 console.log(suma(2,3)); // 5
 */
-
+/*
 const razasPerros = [
     "Pastor Alemán",
     "Labrador Retriever",
@@ -79,3 +79,136 @@ console.log(razaBuscada); // Beagle
 // La estructura es: argumento.findIndex((elemento, indice, arregloOriginal) => { ... })
 const indiceRazaBuscada = razasPerros.findIndex(raza => raza === "Beagle");
 console.log(indiceRazaBuscada); // 3
+*/
+
+// Problema 1
+
+function validarP1(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9]/;
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+} 
+
+function calcularP1() {
+    let x = parseInt(document.getElementById('p1-x').value);
+    let y = parseInt(document.getElementById('p1-y').value);
+    let resultado = x === y ? x * y : x > y ? x - y : x + y;
+    document.getElementById('p1-output').value = resultado;
+    alert("El resultado es: " + resultado);
+}
+
+function borrarP1() {
+    ['p1-x', 'p1-y', 'p1-output'].forEach( id => document.getElementById(id).value = '' );
+}
+
+// Problema 2
+
+function validarP2(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9]/;
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+} 
+
+function calcularP2() {
+    let n1 = parseInt(document.getElementById('p2-n1').value);
+    let n2 = parseInt(document.getElementById('p2-n2').value);
+    let n3 = parseInt(document.getElementById('p2-n3').value);
+    if (n1 === n2 || n1 === n3 || n2 === n3) {
+        document.getElementById('p2-output').value = "No hay número mayor";
+        alert("Los números deben ser distintos");
+    } else {
+        const mayor = Math.max(n1, n2, n3);
+        document.getElementById('p2-output').value = mayor + " es el mayor";
+        alert(mayor + " es el mayor");
+    }
+}
+
+function borrarP2() {
+    ['p2-n1', 'p2-n2', 'p2-n3', 'p2-output'].forEach ( id => document.getElementById(id).value = '' )
+}
+
+// Problema 3
+
+function validarsalP3(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+function validarhrsP3(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9]/;
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+function calcularP3() {
+    const sueldo = parseFloat(document.getElementById('p3-s').value);
+    const horas = parseInt(document.getElementById('p3-h').value);
+    const final = horas <= 8 ? horas * (sueldo * 2) : ((horas - 8) * sueldo * 3) + (sueldo * 16);
+    document.getElementById('p3-output').value = final.toFixed(2);
+    alert("Su salario por horas extra es: " + final.toFixed(2));
+}
+
+function borrarP3() {
+    ['p3-s', 'p3-h', 'p3-output'].forEach( id => document.getElementById(id).value = '' )
+}
+
+// Problema 4
+
+function validarsalP4(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+function validaranioP4(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8) return true;
+    var patron = /[0-9]/;
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
+function calcularP4() {
+    let sueldo = parseFloat(document.getElementById('p4-s').value);
+    let anio = parseInt(document.getElementById('p4-a').value)
+    let final = 0;
+
+    /*
+    const rates = {
+        0: 0.05,
+        1: 0.07,
+        '2-4': 0.1,
+        '5-9': 0.15
+    };
+    final = sueldo * (rates[anio] || rates[`${Math.min(9, Math.max(2, anio))}-${Math.min(9, Math.max(2, anio))}`] || 0.2);
+    */
+
+    if ( anio < 1 ) {
+        final = sueldo * 0.05;
+    } else if ( anio == 1 ) {
+        final = sueldo * 0.07;
+    } else if ( anio >= 2 && anio < 5) {
+        final = sueldo * 0.1;
+    } else if ( anio >= 5 && anio < 10 ) {
+        final = sueldo * 0.15;
+    } else {
+        final = sueldo * 0.2;
+    }
+    document.getElementById('p4-output').value = final.toFixed(2);
+    alert("Sus utilidades son de: $" + final.toFixed(2))
+}
+
+function borrarP4() {
+    ['p4-s', 'p4-a', 'p4-output'].forEach ( id => document.getElementById(id).value = '' )
+}
