186f55ce1511a148e3335cbb44893af0104ed9c3
adan0126 <mgranadosl2301@alumno.ipn.mx>
2025-11-24 13:00:45 -0600

    Se pulleo el trabajo realizado en clase, se revisaron las conexiones a la base de datos y no parece dar errores

--- 14CRUDMySQL/app.js
+++ 14CRUDMySQL/app.js
@@ -16,11 +16,10 @@ const port = 3000;
 
 //configuracion de mysql
 const bd = mysql.createConnection({
-    
-    host: process.env.BD_HOST,
-    user: process.env.BD_USER,
-    password: process.env.BD_PASSWORD,
-    database: process.env.BD_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 bd.connect((error) => {
